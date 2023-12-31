5a45708946c3dc8b8035c8a81b53ee243022f3b6
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-12-31 19:12:11 +0200

    Done

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -1,16 +1,17 @@
 /*
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
-
-function showMessage(message) {
+// Звідки мені знати там повідомлення може бути і число😁,
+//  я би писав unknown але напишу string
+function showMessage(message:string):void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1:number, num2:number):number {
   return num1 + num2;
 }
 
-function customError() {
+function customError():never {
   throw new Error('Error');
 }
 
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,17 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
-
-const page1 = {
+type Post = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+const page1:Post = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +22,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2:Post = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
